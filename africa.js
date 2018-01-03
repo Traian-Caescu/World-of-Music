c148bf6b52509389a49a700d8153431514b5b9b4
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2018-01-03 23:39:32 +0000

    Add files via upload

---
+++ africa.js
@@ -0,0 +1,54 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("ghanaDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("nigeriaDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionMore() {
+    var x = document.getElementById("moreDIV");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunctionMore2() {
+    var x = document.getElementById("more2DIV");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunctionMore3() {
+    var x = document.getElementById("more3DIV");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
